80e408c0b20ee3a4b25c9e622e4e787e74cfee6a
Chantal Snazel <clsnazel@gmail.com>
2020-11-10 22:27:09 +0000

    completed ex1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,18 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+burnaby = Store.new()
+burnaby.name = "Burnaby"
+burnaby.annual_revenue = 300000
+burnaby.womens_apparel = true
+burnaby.mens_apparel = true
+burnaby.save
+
+richmond = Store.new(name:"Richmond", annual_revenue:126000, womens_apparel:true)
+richmond.save
+
+gastown = Store.new(name:"Gastown", annual_revenue:190000, mens_apparel:true)
+gastown.save
+
+puts Store.count
+
