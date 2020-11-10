0bb667f6651fe78e72b68680a16138d3a71be7ad
Chantal Snazel <clsnazel@gmail.com>
2020-11-10 22:48:59 +0000

    completed ex4

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -11,10 +11,10 @@ burnaby.womens_apparel = true
 burnaby.mens_apparel = true
 burnaby.save
 
-richmond = Store.new(name:"Richmond", annual_revenue:126000, womens_apparel:true)
+richmond = Store.new(name:"Richmond", annual_revenue:126000, womens_apparel:true, mens_apparel:false)
 richmond.save
 
-gastown = Store.new(name:"Gastown", annual_revenue:190000, mens_apparel:true)
+gastown = Store.new(name:"Gastown", annual_revenue:190000, mens_apparel:true, womens_apparel:false)
 gastown.save
 
 puts Store.count
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,24 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey = Store.new(name:"Surrey", annual_revenue:224000, womens_apparel:true, mens_apparel:false)
+surrey.save
+
+whistler = Store.new(name:"Whistler", annual_revenue:1900000, mens_apparel:true, womens_apparel:false)
+whistler.save
+
+yaletown = Store.new(name:"Yaletown", annual_revenue:430000, womens_apparel:true, mens_apparel:true)
+yaletown.save
+
+@mens_stores = Store.where(mens_apparel:true, womens_apparel:false)
+
+@mens_stores.each do |store|
+  puts store.name + ": " + store.annual_revenue.to_s
+end
+
+@womens_stores = Store.where(['womens_apparel=true', 'mens_apparel=false', 'annual_revenue < 1000000'])
+
+
+@womens_stores.each do |store|
+  puts store.name + ": " + store.annual_revenue.to_s
+end
